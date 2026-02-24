ceca4cc54d41b86abd4341c87d579a8b25887e7a
Adii-tii <sableaditi8@gmail.com>
2026-02-25 01:00:08 +0530

    set http only cookie (token) via auth middleware so the cookie originates from the API gateway domain

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,6 +4,23 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 const router = Router();
 const AUTH_SERVICE = process.env.AUTH_SERVICE_URL;
 
+
+router.get("/api/auth/success", (req, res) => {
+  const token = req.query.token;
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: false,
+    sameSite: "Lax",
+    path: "/",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
+
+  // important: ensure cookie flushed on 5000 response
+  res.status(302).set("Location", "http://localhost:5173/dashboard").end();
+});
+
+
 router.use(
     "/api/auth",
     createProxyMiddleware({
@@ -16,6 +33,8 @@ router.use(
         },
     })
 );
+
+
 
 router.use(
     "/api/users",
